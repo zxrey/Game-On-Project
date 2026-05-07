f975e16e1a8798124070617da2be24f7966b748a
James Rogers <rogersimy@yahoo.com>
2026-05-07 09:49:42 -0600

    Main que llama archivos de pln_model y carga csv

--- game_on/interface/main.py
+++ game_on/interface/main.py
@@ -1,14 +1,20 @@
-# interface/main.py
-
-# Importa desde la subcarpeta 'pln_model'
-from pln_model.limpieza import procesar_dataset
-from pln_model.sbert import generar_embeddings
+from pln_model.limpieza import limpieza
+from pln_model.sbert import generar_embeddings, query
 
 def main():
-    # Procesamiento y limpieza
-    procesar_dataset()
-    # Generación de embeddings
-    generar_embeddings()
+    # Cargar dataset local, por ejemplo un CSV
+    df = pd.read_csv('/mnt/c/Users/Usuario/Downloads/steam_games.csv')
+
+    # Limpiar dataset
+    data_limpia = limpieza(df1)
+
+    # Generar embeddings con sbert.py
+    embeddings = generar_embeddings(data_limpia)
+
+    # Ejecutar query (consulta)
+    resultado = query("tu consulta", data_limpia, embeddings, n_top=5)
+
+    print(resultado)
 
 if __name__ == "__main__":
     main()
